ba693cff77e0f1349fb0e4361037e38dc5b40a0f
Adamsky94 <a.kiss1994@gmail.com>
2021-01-17 23:44:58 +0000

    Added ZoomJS functionality

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,4 +1,8 @@
-/*navbar*/
+/*Mobile navbar functionality*/
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+/*MediumZoom Javascript https://github.com/francoischalifour/medium-zoom */
+mediumZoom(document.querySelectorAll('[data-zoomable]'),{
+    background: "rgba(255,255,255,.6)"
+})
