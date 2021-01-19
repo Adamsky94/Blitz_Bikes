2ddfa478ddf5469173e1dfe027d7f91cf91ff39f
Adamsky94 <a.kiss1994@gmail.com>
2021-01-19 17:23:00 +0000

    Added scroll to top button

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -12,4 +12,23 @@ $(document).ready(function(){
         background: "rgba(255,255,255,.8)"
     });
 
+    /* Scroll to top button code from @rdallaire on CodePen https://codepen.io/rdallaire/pen/apoyx */
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 600) {
+        // If page is scrolled more than 600px
+        $("#return-to-top").fadeIn(100); // Fade in the arrow
+    } else {
+        $("#return-to-top").fadeOut(100); // Else fade out the arrow
+    }
+});
+$("#return-to-top").click(function () {
+    // When arrow is clicked
+    $("body,html").animate(
+        {
+            scrollTop: 0, // Scroll to top of body
+        },
+        500
+    );
+});
+
 })
